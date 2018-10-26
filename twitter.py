74ab62bfa08342f0faef2779e0e8ccca0a5ecd4a
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-10-26 13:27:11 +0000

    tweet

---
+++ twitter.py
@@ -0,0 +1,10 @@
+import os
+import env
+import json
+import tweepy
+from tweepy import OAuthHandler
+
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY") 
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET") 
+OAUTH_TOKEN = os.environ.get("OAUTH_TOKEN") 
+OAUTH_TOKEN_SECRET = os.environ.get("OAUTH_TOKEN_SECRET") 
